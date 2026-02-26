55be0d42120a185a09164cabb85580264f4d48fb
AbdeDev <98222128+AbdeDev@users.noreply.github.com>
2026-02-26 01:50:42 +0100

    fix : reuse flowtype implicite

--- apps/admin/src/lib/supabase.ts
+++ apps/admin/src/lib/supabase.ts
@@ -16,7 +16,7 @@ if (!url || !key) {
 
 export const supabase = createClient(url, key, {
   auth: {
-    flowType: "pkce",
+    flowType: "implicit",
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: true,
